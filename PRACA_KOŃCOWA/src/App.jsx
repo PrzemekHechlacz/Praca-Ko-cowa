196b01dca10b397344ae85acaca9ee826d2bf5c4
PrzemekHechlacz <p.hechlacz@gmail.com>
2024-02-24 22:44:04 +0100

    end

--- PRACA_KOŃCOWA/src/App.jsx
+++ PRACA_KOŃCOWA/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AboutMe from "./components/AboutMe/AboutMe";
 import Portfolio from "./components/Portfolio/Portfolio";
@@ -7,20 +8,24 @@ import FirstPage from './components/FirstPage/FirstPage';
 import LinkTrening from './components/LinkTrening/LinkTrening';
 import LinkArrange from './components/LinkArrange/LinkArrange';
 
-
 const App = () => {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<FirstPage />} />
-      <Route path="/link-arrange" element={<LinkArrange/>} />
-       <Route path="/link-trening" element={<LinkTrening />} />
+        <Route path="/" element={<FirstPage />} />
+        <Route path="/link-arrange" element={<LinkArrange/>} />
+        <Route path="/link-trening" element={<LinkTrening />} />
         <Route path="/about-me" element={<AboutMe />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
-       </Routes>
+      </Routes>
     </Router>
   );
 };
 
-export default App;
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />);
+
+export default App
--- PRACA_KOŃCOWA/src/main.jsx
+++ PRACA_KOŃCOWA/src/main.jsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import App from './App';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
-
